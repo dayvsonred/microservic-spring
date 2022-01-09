960825448abcf75c6fa0ff823e76c918a51af3a9
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-01-09 01:28:56 -0300

    payroll up ok

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrpayroll.feignclients;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+//@FeignClient(name = "hr-worker", path = "/workers")
+public interface WorkerFeignClient {
+
+//    @GetMapping(value = "/{id}")
+//    ResponseEntity<Worker> findById(@PathVariable Long id);
+}
