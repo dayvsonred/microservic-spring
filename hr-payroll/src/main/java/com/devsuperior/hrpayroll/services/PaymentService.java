2c8c831a5231b78bbce6841c42fb5b974fcf3031
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-02-05 17:01:06 -0300

    config ok

--- hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -1,7 +1,7 @@
 package com.devsuperior.hrpayroll.services;
 
-import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.entities.Worker;
+import com.devsuperior.hrpayroll.entities.Payment;
 import com.devsuperior.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,7 +14,7 @@ public class PaymentService {
 
     public Payment getPayment(long workerId, int days) {
 
-        Worker worker = workerFeignClient.findById(workerId).getBody();
+        Worker worker = this.workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
--- hr-worker/src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/repositories/WorkerRepository.java
@@ -1,8 +1,7 @@
 package com.devsuperior.hrworker.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.devsuperior.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
 
