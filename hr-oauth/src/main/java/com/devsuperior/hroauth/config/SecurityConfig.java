b98efe2b2b28c0ad3eb32a840ad22b8ba2df5e1c
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-02-06 18:40:10 -0300

    config securit

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.devsuperior.hroauth.config;
+
+//@Configuration
+public class SecurityConfig { // extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private BCryptPasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+//
+//    @Override
+//    @Autowired
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+//    }
+//
+//    @Override
+//    @Bean
+//    protected AuthenticationManager authenticationManager() throws Exception {
+//        return super.authenticationManager();
+//    }
+}
