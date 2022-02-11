455514dbc37a96176d0f583ff9b2ecaecf0487ff
dayvson_menezes_local <dayvison.assis@gmail.com>
2022-02-10 21:30:12 -0300

    creat hr-process

---
+++ hr-process/src/main/java/com/devsuperior/hrprocess/HrProcessApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrProcessApplication.class, args);
+	}
+
+}
